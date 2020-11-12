25592d4ad7f91ce47fbf4db5a3dcd69cdc0ef66a
mantou132 <709922234@qq.com>
2020-11-13 04:32:04 +0800

    Fixed cli

--- webpack.config.cli.js
+++ webpack.config.cli.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 /**
@@ -24,6 +25,7 @@ module.exports = {
     path: path.resolve(__dirname, 'bin'),
     filename: 'index.js',
   },
+  plugins: [new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })],
   externals: [nodeExternals()],
   devtool: 'source-map',
 };
