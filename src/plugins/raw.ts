18898c51491944e53ecb503672b81da937fde376
mantou132 <709922234@qq.com>
2021-05-22 17:38:50 +0800

    Plugins use decorator

--- src/plugins/raw.ts
+++ src/plugins/raw.ts
@@ -2,48 +2,46 @@ import type { GemBookElement } from '../element';
 
 customElements.whenDefined('gem-book').then(() => {
   const { GemBookPluginElement } = customElements.get('gem-book') as typeof GemBookElement;
-  const { config } = GemBookPluginElement;
+  const { config, Gem } = GemBookPluginElement;
+  const { attribute, customElement } = Gem;
 
-  customElements.define(
-    'gbp-raw',
-    class extends GemBookPluginElement {
-      static observedAttributes = ['src', 'codelang', 'range', 'highlight'];
-      src: string;
-      codelang: string;
-      range: string;
-      highlight: string;
+  @customElement('gbp-raw')
+  class _GbpRawElement extends GemBookPluginElement {
+    @attribute src: string;
+    @attribute codelang: string;
+    @attribute range: string;
+    @attribute highlight: string;
 
-      mounted() {
-        this.renderContent();
+    mounted() {
+      this.renderContent();
+    }
+
+    updated() {
+      this.renderContent();
+    }
+
+    async renderContent() {
+      if (!this.src) return;
+      this.innerHTML = 'Loading...';
+
+      let url = this.src;
+
+      if (this.src.startsWith('/') && !this.src.startsWith('//')) {
+        if (!config.github || !config.sourceBranch) return;
+
+        const rawOrigin = 'https://raw.githubusercontent.com';
+        const repo = new URL(config.github).pathname;
+        url = `${rawOrigin}${repo}/${config.sourceBranch}${this.src}`;
       }
+      const text = await (await fetch(url)).text();
+      const div = document.createElement('div');
+      div.textContent = text;
+      const content = div.innerHTML;
 
-      updated() {
-        this.renderContent();
-      }
-
-      async renderContent() {
-        if (!this.src) return;
-        this.innerHTML = 'Loading...';
-
-        let url = this.src;
-
-        if (this.src.startsWith('/') && !this.src.startsWith('//')) {
-          if (!config.github || !config.sourceBranch) return;
-
-          const rawOrigin = 'https://raw.githubusercontent.com';
-          const repo = new URL(config.github).pathname;
-          url = `${rawOrigin}${repo}/${config.sourceBranch}${this.src}`;
-        }
-        const text = await (await fetch(url)).text();
-        const div = document.createElement('div');
-        div.textContent = text;
-        const content = div.innerHTML;
-
-        const extension = this.src.split('.').pop() || '';
-        this.innerHTML = `<gem-book-pre codelang="${this.codelang || extension}" highlight="${this.highlight}" range="${
-          this.range
-        }">${content}</gem-book-pre>`;
-      }
-    },
-  );
+      const extension = this.src.split('.').pop() || '';
+      this.innerHTML = `<gem-book-pre codelang="${this.codelang || extension}" highlight="${this.highlight}" range="${
+        this.range
+      }">${content}</gem-book-pre>`;
+    }
+  }
 });
